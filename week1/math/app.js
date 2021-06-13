d1bb838e7b80f9b2d90a9d987a8fdc3a02427757
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-06-12 23:49:54 -0700

    Math

---
+++ week1/math/app.js
@@ -0,0 +1,10 @@
+const mathObj = require('./math')
+
+console.log(mathObj);
+console.log(mathObj.sum(10,2));
+console.log(mathObj.subtract(7,5));
+console.log(mathObj.multiply(25,5));
+console.log(mathObj.divide(100,10));
+console.log(mathObj.sum(22, 44));
+console.log(mathObj.divide(75, 5));
+console.log(mathObj.subtract(56, 32));
